2dba96a320c32ff158a76bdbc07941138ff710f6
Kazakoff <wadimchik@mail.com>
2025-06-09 10:53:40 +0300

    header merge conflict fixed

--- src/app.js
+++ src/app.js
@@ -7,26 +7,25 @@ const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { notFound, errorHandler, timeSign } = require('./middlewares');
 
-
 const app = express();
 
 console.log(`${__dirname}/ui-routes/views`);
 
+app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/ui-routes/views`);
 app.use(express.static(`${__dirname}/ui-routes/views`));
 
 app.use(express.static(`${__dirname}/ui-routes/public`));
-app.set('view engine', 'ejs');
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(timeSign);
 
-app.set('views', `${__dirname}/ui-routes/views`);
-app.use(express.static(`${__dirname}/ui-routes/public`));
+// app.set('views', `${__dirname}/ui-routes/views`);
+// app.use(express.static(`${__dirname}/ui-routes/public`));
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 const employees = require('./routes/employees');
 const ui = require('./ui-routes/index');
